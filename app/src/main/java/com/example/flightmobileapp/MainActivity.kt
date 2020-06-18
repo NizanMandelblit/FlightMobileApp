26a44b5291e3d391c13870b6fb10a7645cee84e5
Eldad Horvitz <eldad373@gmail.com>
2020-06-18 12:41:21 +0300

    cant create database

--- app/src/main/java/com/example/flightmobileapp/MainActivity.kt
+++ app/src/main/java/com/example/flightmobileapp/MainActivity.kt
@@ -3,14 +3,15 @@ package com.example.flightmobileapp
 import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import kotlinx.android.synthetic.main.activity_main.*
+import com.example.flightmobileapp.UrlDatabase.Companion
 
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
         ConnectButton.setOnClickListener() {
             val database = getSharedPreferences("datebase", Context.MODE_PRIVATE)
             database.edit().apply() {
@@ -20,7 +21,11 @@ class MainActivity : AppCompatActivity() {
             button.setOnClickListener() {
                 UrlInput.setText(database.getString("url", "no value"))
             }
+            //Urls1.get(application)
         }
+        //print("HELLO TRYING TO PRINT")
+        //Log.d("PRINT", "Printing this message")
+        //UrlDatabase.get(application)
 
 
     }
